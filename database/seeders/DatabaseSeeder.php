9271941fb7cab00d2f586959ca88e6ed91b698f8
Andrew Peroramas <wolfandrew307@gmail.com>
2025-03-13 01:00:03 +0800

    test: add test seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Actions\AddUserQueue;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,19 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Olaf',
+            'email' => 'admin@kewlor.com',
+            'password' => 'A123jkl$*9p$)',
         ]);
+
+        $addUserQueue = app()->make(AddUserQueue::class);
+        for ($i = 0; $i < 10; $i++) {
+
+            $addUserQueue->handle([
+                'name' => fake()->userName(),
+                'email' => fake()->safeEmail(),
+                'message' => fake()->words(5, true),
+            ]);
+        }
     }
 }
