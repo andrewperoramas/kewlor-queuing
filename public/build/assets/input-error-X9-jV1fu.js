0dc191192a873127692dd85e6839a524eb2c9770
Andrew Peroramas <wolfandrew307@gmail.com>
2025-03-20 06:05:09 +0800

    chore: add build

---
+++ public/build/assets/input-error-X9-jV1fu.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-GEt8cp3Q.js";import{c as o}from"./button-ni_ZfWX1.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
