abc857b8995b30bb7a3cd3d283246ff4ea1d5730
Andrew Peroramas <wolfandrew307@gmail.com>
2025-04-21 12:35:09 +0800

    add build

---
+++ public/build/assets/input-error-C_veRqo8.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-2lJiJyW1.js";import{c as o}from"./button-BkhR1CmZ.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
